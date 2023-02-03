0d2ec03a426534d6264b4c14402b9938560e1f17
mrKrivedko <mr.krivedko.ds@gmail.com>
2023-02-03 14:35:39 +0600

    test request.get on test-client socket

--- test-client.py
+++ test-client.py
@@ -2,20 +2,20 @@ import socket
 
 # Подключаем сокет к порту, через 
 # который прослушивается сервер
-server_address = ('localhost', 8080)
+server_address = ('swapi.dev', 80)
 with socket.create_connection(server_address) as sock:
     print('Подключено к:', server_address)
     # Отправка данных
-    mess = 'Привет!'
+    mess = 'hello! this ilon mask fucked day now'
     print(f'Отправка: {mess}')
-    message = mess.encode()
+    message = b'GET /api/ HTTP/1.1\r\nHost:www.swapi.dev\r\n\r\n'
     sock.sendall(message)
     
     # Смотрим ответ
     amount_received = 0
     amount_expected = len(message)
     while amount_received < amount_expected:
-        data = sock.recv(16)
+        data = sock.recv(4096)
         amount_received += len(data)
         mess = data.decode()
         print(f'Получено: {data.decode()}')
